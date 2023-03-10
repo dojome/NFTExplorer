e2d32099630840e229ad61f655154a5c9d9f581a
bingo526 <chief.victoru@gmail.com>
2023-03-10 07:59:16 -0800

    update logo link

--- src/components/Logo/index.js
+++ src/components/Logo/index.js
@@ -2,5 +2,9 @@ import React from 'react'
 import LogoImg from '../../assets/logo.png'
 
 export function Logo() {
-  return <img src={LogoImg} width="50" alt="logo" />
+  return (
+    <a href="https://www.alturanft.com/">
+      <img src={LogoImg} width="50" alt="logo" />
+    </a>
+  )
 }

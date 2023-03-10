da7bbbaa9c5788f66cf2001a492a334c78ff2719
bingo526 <chief.victoru@gmail.com>
2023-03-10 06:17:23 -0800

    update media component

--- src/components/Media/index.js
+++ src/components/Media/index.js
@@ -7,7 +7,7 @@ export function Media({ nft }) {
     case 'mp4':
       return (
         <video width="350" height="350" className="h-60" controls autoPlay loop alt={mediaType}>
-          <source src={nft?.media[0]?.raw} type="video/mp4" />
+          <source src={nft?.media[0]?.gateway || nft?.media[0]?.raw} type="video/mp4" />
         </video>
       )
     case 'png' || 'jpeg' || 'jpg' || 'gif':
